010f2543b26f3f452cd53b283c562cc67fa782b2
Raquel Buege <rlb143@pitt.edu>
2025-02-01 10:07:07 -0500

    nav bar and profile

--- milestones/lib/profile.dart
+++ milestones/lib/profile.dart
@@ -6,16 +6,43 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xffC4DFCB),
+      color: Color.fromARGB(255, 255, 255, 255),
+      child: 
+      Padding(
+      padding:  EdgeInsets.fromLTRB(0, 0, 0, 40),
       child: Center(
-        child: Text(
-          "Profile",
-          style: TextStyle(
-            color: Colors.green[900],
-            fontSize: 45,
-            fontWeight: FontWeight.w500,
-          ),
+          child: Container(
+        width: 380,
+        height: 700,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12), // Optional rounded corners
+          color: Color(0xFF1B1C3A), // Background color
         ),
+      child: const Column(
+
+        children: [
+          
+          Padding(
+            padding:  EdgeInsets.fromLTRB(0, 40, 200, 0),
+            child: Text(
+          style:TextStyle(
+            color: Color(0xFFF0E9DA),
+            fontSize: 35,
+            fontWeight: FontWeight.bold
+          ),
+            'Profile')),
+            Padding(
+               padding:  EdgeInsets.fromLTRB(0,10, 200, 0),
+            child: Icon(
+                    Icons.person  ,
+                    color: Color(0xFFF0E9DA),
+                    size: 80,
+                  )
+            ),
+        ],
+      ),
+      
+      ))
       ),
     );
   }
